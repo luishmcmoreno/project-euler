4fbac5b462ed4c08afde4e3a49a6a00b95c159cb
Luis H. Moreno <luishmcmoreno@gmail.com>
2017-10-13 18:52:33 -0300

    problem 0067

---
+++ 0067.py
@@ -0,0 +1,25 @@
+INFINITY = 999999999999999999
+
+mat = []
+
+file = open('p067_triangle.txt', 'r')
+i = 0
+for line in file.readlines():
+    currentLine = line.split(' ')
+    mat.append([])
+    j = 0
+    for number in currentLine:
+        mat[i].append(int(number))
+        j += 1
+    i += 1
+
+matlen = len(mat)
+
+for i in range(matlen - 2, -1, -1):
+  for j in range(matlen - (matlen - i - 1)):
+    if (j + 1 < matlen):
+      mat[i][j] = max(mat[i][j] + mat[i+1][j], mat[i][j] + mat[i+1][j + 1])
+    else:
+      mat[i][j] = mat[i][j] + mat[i+1][j]
+
+print mat[i][j]
